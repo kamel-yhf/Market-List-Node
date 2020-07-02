f17ec7604b21f0e041a0e937ecb027985b9df139
kamel-yhf <kamelyakhlef@hotmail.fr>
2020-07-02 14:06:19 +0200

    pricesProducts ok

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const markets = require("./routes/markets");
 app.use('/markets', markets);
 const products = require("./routes/products");
 app.use('/products', products);
+const pricesProducts = require("./routes/pricesProducts");
+app.use('/pricesProducts', pricesProducts);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {console.log(`server OK ${port}`)});
