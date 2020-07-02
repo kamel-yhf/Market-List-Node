42c03b0931ffd8e433bbae11603bb2f7e33cbee5
kamel-yhf <kamelyakhlef@hotmail.fr>
2020-07-02 10:33:25 +0200

    router.get test

--- routes/lists.js
+++ routes/lists.js
@@ -2,13 +2,22 @@ const express = require('express');
 const mongodb = require('mongodb');
 
 const router = express.Router();
+const uri = 'mongodb+srv://Kamel:679344856@cluster0-boqd6.mongodb.net/marketdb?retryWrites=true&w=majority';
 
 //getList
-router.get('/', (req, res) => {
-    res.send("hello");
+router.get('/', async (req, res) => {
+    const lists = await loadLists();
+    res.send(await lists.find({}).toArray());
 });
 //addList
 
+
 //deleteList
+
+async function loadLists(){
+    const client = await mongodb.MongoClient.connect(
+        uri,{useNewUrlParser: true});
+    return client.db('marketdb').collection('lists');
+}
 
 module.exports = router;
