5abb15df234023d4961e42a1917753a262e737dd
kamel-yhf <kamelyakhlef@hotmail.fr>
2020-07-28 14:35:14 +0200

    add list on userShema

--- models/Users.js
+++ models/Users.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-
+const lists = require('./Lists');
+const listSchema = mongoose.model('Lists').schema;
 
 const UsersShema = mongoose.Schema({
     userName: {type: String, required: true},
@@ -7,7 +8,8 @@ const UsersShema = mongoose.Schema({
     email: {type: String, required: true, unique: true},
     password: {type: String, required: true},
     phone: {type: String, required: true},
-    address: {type: String, required: true}
+    address: {type: String, required: true},
+    lists: {type: [listSchema]}
 });
 
 // export ma methode en indiquant à mongoose le format
